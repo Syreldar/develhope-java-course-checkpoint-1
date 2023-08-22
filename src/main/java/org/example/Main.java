679a7e7556f816a37f363e663a6a062532472800
Syreldar <enrico.drago3@gmail.com>
2023-08-22 17:50:45 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,34 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        Product[] products =
+        {
+            new Product(1000000, "Telefono", "Nokia", 20),
+            new Product(1000001, "Bottiglia", "Fontenoce", 30),
+            new Product(1000002, "Portafoglio", "Lacoste", 50)
+        };
+
+        printManufacturerFirstLetters(products);
+
+        int totalQuantity = 0;
+        for (Product product : products)
+            totalQuantity += product.getQuantity();
+
+        double averageValue = (double) totalQuantity / products.length;
+        System.out.printf("%.2f%n", averageValue);
+        Assertions.assertEquals(33.33, averageValue, 0.01);
+    }
+
+    public static void printManufacturerFirstLetters(Product[] products)
+    {
+        char[] expectedChars = {'N', 'F', 'L', 'C'};
+
+        //for (Product product : products)
+        for (int i = 0; i < products.length; i++)
+        {
+            char firstChar = products[i].getManufacturer().charAt(0);
+            System.out.println(firstChar);
+            Assertions.assertEquals(firstChar, expectedChars[i]);
+        }
     }
 }
