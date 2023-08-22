8d3025996c97b71805e5667720a41627fe48cb3f
Syreldar <enrico.drago3@gmail.com>
2023-08-22 18:03:08 +0200

    Removed Assertions debug.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import org.junit.jupiter.api.Assertions;
-
 public class Main
 {
     public static void main(String[] args)
@@ -21,20 +19,11 @@ public class Main
 
         double averageValue = (double) totalQuantity / products.length;
         System.out.printf("%.2f%n", averageValue);
-        Assertions.assertEquals(33.33, averageValue, 0.01);
     }
 
     public static void printManufacturerFirstLetters(Product[] products)
     {
-        char[] expectedChars = {'N', 'F', 'L', 'C'};
-
-        //for (Product product : products)
-        char firstChar;
-        for (int i = 0; i < products.length; i++)
-        {
-            firstChar = products[i].getManufacturer().charAt(0);
-            System.out.println(firstChar);
-            Assertions.assertEquals(firstChar, expectedChars[i]);
-        }
+        for (Product product : products)
+            System.out.println(product.getManufacturer().charAt(0));
     }
 }
